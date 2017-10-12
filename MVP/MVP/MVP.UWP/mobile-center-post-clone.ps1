be98355cb37dfe961e08cb606de8773f9d123946
James Montemagno <james.montemagno@gmail.com>
2017-10-12 14:17:52 -0700

    update uwp script

---
+++ MVP/MVP/MVP.UWP/mobile-center-post-clone.ps1
@@ -0,0 +1,15 @@
+print "Variables:"
+printenv
+
+print "Arguments for updating:"
+print " - MCID: $MC_UWP"
+print " - Live ID: $LIVE_ID"
+print " - Live Secret: $LIVE_SECRET"
+
+# Updating manifest
+(Get-Content ..\MVP\Helpers\CommonConstants.cs).replace('MC_UWP', $MC_UWP) | Set-Content ..\MVP\Helpers\CommonConstants.cs
+(Get-Content ..\MVP\Helpers\CommonConstants.cs).replace('LIVE_ID', $LIVE_ID) | Set-Content ..\MVP\Helpers\CommonConstants.cs
+(Get-Content ..\MVP\Helpers\CommonConstants.cs).replace('LIVE_SECRET', $LIVE_SECRET) | Set-Content ..\MVP\Helpers\CommonConstants.cs
+
+
+print "Manifest updated!"
