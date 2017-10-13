0acc85cf4bbdfbcc57cd0206fe4ae7a5fc9b41ca
James Montemagno <james.montemagno@gmail.com>
2017-10-12 17:33:07 -0700

    fix sed again

--- MVP/MVP/MVP.Droid/mobile-center-post-clone.sh
+++ MVP/MVP/MVP.Droid/mobile-center-post-clone.sh
@@ -9,9 +9,9 @@ echo " - Live ID: $LIVE_ID"
 echo " - Live Secret: $LIVE_SECRET"
 
 # Updating manifest
-sed -i "s/MC_ANDROID/$MC_ANDROID/g" $BUILD_REPOSITORY_LOCALPATH/MVP/MVP/MVP/Helpers/CommonConstants.cs
-sed -i "s/LIVE_ID/$LIVE_ID/g" $BUILD_REPOSITORY_LOCALPATH/MVP/MVP/MVP/Helpers/CommonConstants.cs
-sed -i "s/LIVE_SECRET/$LIVE_SECRET/g" $BUILD_REPOSITORY_LOCALPATH/MVP/MVP/MVP/Helpers/CommonConstants.cs
+sed -i '' "s/MC_ANDROID/$MC_ANDROID/g" $BUILD_REPOSITORY_LOCALPATH/MVP/MVP/MVP/Helpers/CommonConstants.cs
+sed -i '' "s/LIVE_ID/$LIVE_ID/g" $BUILD_REPOSITORY_LOCALPATH/MVP/MVP/MVP/Helpers/CommonConstants.cs
+sed -i '' "s/LIVE_SECRET/$LIVE_SECRET/g" $BUILD_REPOSITORY_LOCALPATH/MVP/MVP/MVP/Helpers/CommonConstants.cs
 
 cat $BUILD_REPOSITORY_LOCALPATH/MVP/MVP/MVP/Helpers/CommonConstants.cs
 
