1a89acb88bc792dc3d40bc6f1e3766cffdaff2f7
James Montemagno <james.montemagno@gmail.com>
2017-10-12 17:08:40 -0700

    Update all build scripts

--- MVP/MVP/MVP.Droid/mobile-center-post-clone.sh
+++ MVP/MVP/MVP.Droid/mobile-center-post-clone.sh
@@ -13,4 +13,6 @@ sed -i '' 's/MC_ANDROID/$MC_ANDROID/g' $BUILD_REPOSITORY_LOCALPATH/MVP/MVP/MVP/H
 sed -i '' 's/LIVE_ID/$LIVE_ID/g' $BUILD_REPOSITORY_LOCALPATH/MVP/MVP/MVP/Helpers/CommonConstants.cs
 sed -i '' 's/LIVE_SECRET/$LIVE_SECRET/g' $BUILD_REPOSITORY_LOCALPATH/MVP/MVP/MVP/Helpers/CommonConstants.cs
 
+cat $BUILD_REPOSITORY_LOCALPATH/MVP/MVP/MVP/Helpers/CommonConstants.cs
+
 echo "Manifest updated!"
--- MVP/mobile-center-post-clone.ps1
+++ MVP/mobile-center-post-clone.ps1
@@ -7,9 +7,11 @@ print " - Live ID: $LIVE_ID"
 print " - Live Secret: $LIVE_SECRET"
 
 # Updating manifest
-(Get-Content MVP\MVP\Helpers\CommonConstants.cs).replace('MC_UWP', $MC_UWP) | Set-Content MVP\MVP\Helpers\CommonConstants.cs
-(Get-Content MVP\MVP\Helpers\CommonConstants.cs).replace('LIVE_ID', $LIVE_ID) | Set-Content MVP\MVP\Helpers\CommonConstants.cs
-(Get-Content MVP\MVP\Helpers\CommonConstants.cs).replace('LIVE_SECRET', $LIVE_SECRET) | Set-Content MVP\MVP\Helpers\CommonConstants.cs
+(Get-Content $BUILD_REPOSITORY_LOCALPATH\MVP\MVP\MVP\Helpers\CommonConstants.cs).replace('MC_UWP', $MC_UWP) | Set-Content $BUILD_REPOSITORY_LOCALPATH\MVP\MVP\MVP\Helpers\CommonConstants.cs
+(Get-Content $BUILD_REPOSITORY_LOCALPATH\MVP\MVP\MVP\Helpers\CommonConstants.cs).replace('LIVE_ID', $LIVE_ID) | Set-Content $BUILD_REPOSITORY_LOCALPATH\MVP\MVP\MVP\Helpers\CommonConstants.cs
+(Get-Content $BUILD_REPOSITORY_LOCALPATH\MVP\MVP\MVP\Helpers\CommonConstants.cs).replace('LIVE_SECRET', $LIVE_SECRET) | Set-Content $BUILD_REPOSITORY_LOCALPATH\MVP\MVP\MVP\Helpers\CommonConstants.cs
 
+
+Get-Content $BUILD_REPOSITORY_LOCALPATH\MVP\MVP\MVP\Helpers\CommonConstants.cs | Out-Printer
 
 print "Manifest updated!"
