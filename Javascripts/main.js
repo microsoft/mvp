f579c0e60692237830456bc4db4f0a4bdcb840a6
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-10-14 23:15:41 +0200

    Show random personas in the portrait mode
    This commit makes sure that random persona is shown on small screens by picking
    one dominant person randomly and adjusting background image position accordingly.
    
    Thanks!

--- Javascripts/main.js
+++ Javascripts/main.js
@@ -5,5 +5,7 @@
         if (window.interactions) {
             window.interactions.setup();
         }
+        var pickOne = Math.floor(Math.random() * 3 + 1);
+        $('.wrap').addClass('pick-' + pickOne);
     });
 }(jQuery));
